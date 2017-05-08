4d0eb13d4c114493cade15d7ff477e9ba3fc94b8
beckg2 <beckg2@seattleu.edu>
2017-05-08 17:03:47 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
-/* Custom JS goes here. */
+// Document ready handler
+$(document).on('ready', function(){
+    // Form validation
+    $('#order-form').validate({
+        submitHandler: function(form) {
+            // If valid, then submit
+            form.submit();
+        },
+        rules: 
+            //check name field is filled out with max characters of 128
+            {
+            "your-name": {
+                required: true,
+                maxlength: 128,
+            },
+            //check state field is filled out with max characters of 2
+            "your-state": {
+                required: true,
+                maxlength: 2
+            },
+            //check zip field is filled out with max characters of 5 and only using numbers
+            "your-zip": {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            //check card name field is filled out with max characters of 128
+            "card-holder-name": {
+                required: true,
+                maxlength: 128
+            },
+            //check card numer field is filled out and making sure it is a credit card
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+            //check 3digit code on back of card field is filled out with max characters of 3 and only using numbers
+            "cvv": {
+                required: true,
+                maxlength: 3,
+                digits: true
+            }
+        }
+    });
+    // Tooltips
+    $('label span.glyphicon').tooltip();
+});
 
-// For this assignment you'll need to do a few things:
-// 1. Create a document ready handler.
-// 2. Define a validation object for use on your page.
-// 3. Connect the validation object to an event handler tied to the submit button.
 
-// Refer to the `index.html` file for the validation rules that must be enforced.
+
+
+
+
+
